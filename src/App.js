42423229b2e98020db69e4144066acfac1d421a5
Dipansu Rout <dip9381@gmail.com>
2023-06-17 13:36:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Register from "./components/Register.jsx";
 import './components/css/style.css';
 import Login from './components/Login.jsx';
 import Home from './components/Home.jsx';
-import { HashRouter,Routes,Route,Navigate } from "react-router-dom";
+import { BrowserRouter,Routes,Route,Navigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./components/context/Authcontext.jsx";
 function App() {
@@ -17,15 +17,15 @@ function App() {
   }
   return (
     <>
-    <HashRouter basename="/weebChat">
+    <BrowserRouter basename="/weebChat">
     <Routes>
       <Route path="/home" element={<Securelogout>
         <Home />
       </Securelogout>}></Route>
-      <Route path="/" element={<Login/>}></Route>
+      <Route exact path="/weebChat" element={<Login/>}></Route>
       <Route path="/register" element={<Register/>}></Route>
     </Routes>
-    </HashRouter>
+    </BrowserRouter>
     </>
   );
 }
