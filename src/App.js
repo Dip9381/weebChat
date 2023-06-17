a57847cbe2fe61522416391ac281a4b8a32360e5
Dipansu Rout <dip9381@gmail.com>
2023-06-17 14:16:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import Register from "./components/Register.jsx";
 import './components/css/style.css';
 import Login from './components/Login.jsx';
 import Home from './components/Home.jsx';
-import { BrowserRouter,Routes,Route,Navigate } from "react-router-dom";
+import { HashRouter,Routes,Route,Navigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./components/context/Authcontext.jsx";
 function App() {
   const {curruser}=useContext(AuthContext);
   const Securelogout =({children})=>{
     if(!curruser){
-     return <Navigate to='/weebChat'/>
+     return <Navigate to='/'/>
     }
     else{
     return  children;
@@ -17,7 +17,7 @@ function App() {
   }
   return (
     <>
-    <BrowserRouter basename="/weebChat">
+    <HashRouter basename="/weebChat">
     <Routes>
       <Route path="/home" element={<Securelogout>
         <Home />
@@ -25,7 +25,7 @@ function App() {
       <Route exact path="/weebChat" element={<Login/>}></Route>
       <Route path="/register" element={<Register/>}></Route>
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </>
   );
 }
